440c62264dfb6f9d07b24f34de44159fd7262f62
Jailson Marques <jailsonmarquesjm@hotmail.com>
2024-06-16 21:06:35 -0300

    operadores logicos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero1 = 2;
+        int numero2 = 2;
+
+        if (numero1 < numero2) {
+            System.out.println("O numero 1 é menor que o numero 2!");
+        } else if (numero1 > numero2) {
+            System.out.println("O numero 1 é maior que o numero 2!");
+        } else {
+            System.out.println("Os numeros são iguais.");
+        }
     }
 }
